4036c23a64ca183da5ae8f7d076d609213d8400b
DmitryMass <dimamikron41@gmail.com>
2023-05-25 14:36:11 +0300

    added sort for Name category as alphabet

--- utils/usersSort.ts
+++ utils/usersSort.ts
@@ -5,6 +5,5 @@ export const usersSortFn = {
   Age: (a: UserData, b: UserData) => a.age - b.age,
   Height: (a: UserData, b: UserData) => a.height - b.height,
   Weight: (a: UserData, b: UserData) => a.weight - b.weight,
-  Name: (a: UserData, b: UserData) => a.weight,
+  Name: (a: UserData, b: UserData) => a.firstName.localeCompare(b.firstName),
 };
-// почитать соритровку по алфавиту
